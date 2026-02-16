9288a56468eb5ecd6e0fb9b34c456d83d819949c
sairamyedida06 <sairamyedidaoffl@gmail.com>
2026-02-16 14:05:02 +0530

    feat(Player): add player health system
    Implemented health system for player using Idamagable Interface.
    player looses all the health if he falls below allocated y position value.

--- Virus Escape/Assets/Scripts/Player/Player_Health.cs
+++ Virus Escape/Assets/Scripts/Player/Player_Health.cs
@@ -1,16 +1,63 @@
+using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UIElements;
 
-public class Player_Health : MonoBehaviour
+public class Player_Health : MonoBehaviour,IDamageable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] int maxHealthPoints;
+
+    public int healthPoints
     {
-        
+        get
+        {
+            return maxHealthPoints;
+        }
+        set 
+        {
+            bool wasAlive = maxHealthPoints > 0;
+            maxHealthPoints = value;
+
+            if(healthPoints < 0 && wasAlive)
+            {
+                Died.Invoke();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool alive => healthPoints > 0;
+
+
+    private void Update()
     {
-        
+        if (alive)
+        {
+            if (transform.position.y < -1f)
+            {
+                OnFall();
+
+            }
+        }
+    }
+
+    private void OnFall()
+    {
+        Damage(healthPoints);
+    }
+
+    UnityEvent Damaged;
+    UnityEvent Died;
+
+    public void Damage(int damage)
+    {
+        healthPoints -= damage;
+
+        if (alive)
+        {
+            Damaged.Invoke();
+        }
+       
     }
 }
