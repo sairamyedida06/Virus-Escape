c67e9782bfe2fc581e9341267dceebb2622f03ec
sairamyedida06 <sairamyedidaoffl@gmail.com>
2026-02-24 16:48:19 +0530

    feat(player): rotate player toward movement direction

--- Virus_Escape/Assets/Scripts/Player/Player.cs
+++ Virus_Escape/Assets/Scripts/Player/Player.cs
@@ -5,9 +5,11 @@ public class Player : MonoBehaviour
 {
     [SerializeField] CharacterController characterController;
 
-    Vector2 moveInput;
+    public Vector2 moveInput;
 
-    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] private float moveSpeed = 1f;
+
+    [SerializeField] private float turnSpeed;
 
 
 
@@ -33,9 +35,40 @@ public class Player : MonoBehaviour
         Vector3 motion = moveInput3D * moveSpeed * Time.deltaTime;
 
         characterController.Move(motion);
+
+        UpdatePlayerRotation(moveInput3D);
+
+        
     }
 
     //Rotation
+    void UpdatePlayerRotation(Vector3 moveInput)
+    {
+        if (moveInput.sqrMagnitude <= 0.01f)
+        {
+            return;
+        }
+
+
+        Vector3 playerRotation = transform.rotation.eulerAngles;
+
+        playerRotation.y = GetAngleFromVector(moveInput);
+
+        Quaternion targetRotation = Quaternion.Euler(playerRotation);
+
+        float maxDegrees = turnSpeed * Time.deltaTime;
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, maxDegrees);
+
+    }
+    float GetAngleFromVector(Vector3 direction)
+    {
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+
+        return rotation.eulerAngles.y;
+
+    }
+}
 
     //Gravity
 
@@ -44,4 +77,4 @@ public class Player : MonoBehaviour
     //land detection
 
 
-}
+
