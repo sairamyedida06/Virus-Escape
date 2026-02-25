ecf8d32c46af27f5b180aa56d36899bf897b5a16
sairamyedida06 <sairamyedidaoffl@gmail.com>
2026-02-25 18:08:03 +0530

    feat(health): implement base health component

---
+++ Virus_Escape/Assets/Scripts/Player/Player_Health.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player_Health : MonoBehaviour
+{
+    [SerializeField] int maxHealthPoints;
+
+
+
+    private void Update()
+    {
+        Fall();
+    }
+
+
+    public int HealthPoints
+    {
+        get
+        {
+            return maxHealthPoints;
+        }
+        set
+        {
+            bool wasAlive = HealthPoints > 0;
+            maxHealthPoints = value;
+
+            if (wasAlive && HealthPoints <= 0)
+            {
+                Died.Invoke();
+            }
+        }
+    } 
+        public UnityEvent Died;
+        public UnityEvent Damaged;
+
+    public bool Alive => HealthPoints > 0;
+
+    void Fall()
+    {
+        if (Alive) 
+        {
+            if (transform.position.y <= -2f)
+            {
+                Damage(HealthPoints);
+            }
+        }
+        
+    }
+    public void Damage(int damage)
+    {
+         HealthPoints =- damage;
+    }
+}
